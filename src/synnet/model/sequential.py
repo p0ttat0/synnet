35ae1e08f7094a55a1f2cfecca4b15052e5c34fe
p0ttat0 <ericlin.zip@gmail.com>
2025-09-09 14:39:58 -0400

    ai finished save load

--- src/synnet/model/sequential.py
+++ src/synnet/model/sequential.py
@@ -9,6 +9,11 @@ from synnet.optimizers.no_optimizer import NoOptimizer
 import numpy as np
 import onnx
 from synnet.model.helpers.training_display import ProgressBar
+from synnet.layers.reshape import Reshape
+from synnet.layers.dropout import Dropout
+from synnet.layers.pool import Pool
+from synnet.layers.convolution import Conv
+from synnet.layers.flatten import Flatten
 from synnet.layers.dense import Dense
 
 
@@ -208,8 +213,7 @@ class Sequential:
                 )
                 layer_input = activation_output
             else:
-                print(f"Skipping layer of type {type(layer)} as it is not supported for ONNX export.")
-
+                nodes, initializers, layer_input = self._layer_to_onnx(layer, i, layer_input, initializers)
 
         # Create the graph
         graph_def = onnx.helper.make_graph(
@@ -224,6 +228,128 @@ class Sequential:
         model_def = onnx.helper.make_model(graph_def, producer_name="synnet")
         onnx.save(model_def, file_path)
 
+    def _layer_to_onnx(self, layer, i, layer_input, initializers):
+        nodes = []
+        if isinstance(layer, Flatten):
+            flatten_output = f"flatten_output_{i}"
+            nodes.append(
+                onnx.helper.make_node(
+                    "Flatten",
+                    [layer_input],
+                    [flatten_output],
+                    name=f"Flatten_{i}",
+                )
+            )
+            layer_input = flatten_output
+        elif isinstance(layer, Conv):
+            # Weights
+            weight_name = f"W_{i}"
+            initializers.append(
+                onnx.helper.make_tensor(
+                    name=weight_name,
+                    data_type=self._np_to_onnx_type(layer.weights.dtype),
+                    dims=layer.weights.shape,
+                    vals=layer.weights.flatten().tolist(),
+                )
+            )
+
+            # Conv
+            conv_output = f"conv_output_{i}"
+            nodes.append(
+                onnx.helper.make_node(
+                    "Conv",
+                    [layer_input, weight_name],
+                    [conv_output],
+                    name=f"Conv_{i}",
+                    strides=layer.stride,
+                    pads=[layer.padding[0], layer.padding[1], layer.padding[0], layer.padding[1]],
+                )
+            )
+
+            # Bias
+            bias_name = f"B_{i}"
+            initializers.append(
+                onnx.helper.make_tensor(
+                    name=bias_name,
+                    data_type=self._np_to_onnx_type(layer.bias.dtype),
+                    dims=(1, layer.bias.shape[0], 1, 1),
+                    vals=layer.bias.flatten().tolist(),
+                )
+            )
+
+            # Add
+            add_output = f"add_output_{i}"
+            nodes.append(
+                onnx.helper.make_node(
+                    "Add",
+                    [conv_output, bias_name],
+                    [add_output],
+                    name=f"Add_{i}",
+                )
+            )
+
+            # Activation
+            activation_output = f"activation_output_{i}"
+            nodes.append(
+                onnx.helper.make_node(
+                    layer.act_func.capitalize(),
+                    [add_output],
+                    [activation_output],
+                    name=f"Activation_{i}",
+                )
+            )
+            layer_input = activation_output
+        elif isinstance(layer, Pool):
+            pool_output = f"pool_output_{i}"
+            pool_type = "MaxPool" if layer.pool_mode == "max" else "AveragePool"
+            nodes.append(
+                onnx.helper.make_node(
+                    pool_type,
+                    [layer_input],
+                    [pool_output],
+                    name=f"Pool_{i}",
+                    kernel_shape=[layer.kernel_size, layer.kernel_size],
+                    strides=layer.stride,
+                    pads=[layer.padding[0], layer.padding[1], layer.padding[0], layer.padding[1]],
+                )
+            )
+            layer_input = pool_output
+        elif isinstance(layer, Dropout):
+            dropout_output = f"dropout_output_{i}"
+            nodes.append(
+                onnx.helper.make_node(
+                    "Dropout",
+                    [layer_input],
+                    [dropout_output],
+                    name=f"Dropout_{i}",
+                    ratio=layer.dropout_rate,
+                )
+            )
+            layer_input = dropout_output
+        elif isinstance(layer, Reshape):
+            reshape_output = f"reshape_output_{i}"
+            shape_name = f"shape_{i}"
+            initializers.append(
+                onnx.helper.make_tensor(
+                    name=shape_name,
+                    data_type=onnx.TensorProto.INT64,
+                    dims=(len(layer.output_shape) + 1,),
+                    vals=[-1] + list(layer.output_shape),
+                )
+            )
+            nodes.append(
+                onnx.helper.make_node(
+                    "Reshape",
+                    [layer_input, shape_name],
+                    [reshape_output],
+                    name=f"Reshape_{i}",
+                )
+            )
+            layer_input = reshape_output
+        else:
+            print(f"Skipping layer of type {type(layer)} as it is not supported for ONNX export.")
+        return nodes, initializers, layer_input
+
     @staticmethod
     def load_onnx(file_path: str):
         model = onnx.load(file_path)
@@ -231,6 +357,47 @@ class Sequential:
         # This is a simplified loader and will not work for complex models
         # It assumes a simple sequential model with Dense layers
         for node in model.graph.node:
+            if node.op_type == "Flatten":
+                layers.append(Flatten())
+            if node.op_type == "Conv":
+                weights = None
+                bias = None
+                for initializer in model.graph.initializer:
+                    if initializer.name == node.input[1]:
+                        weights = onnx.numpy_helper.to_array(initializer)
+
+                add_node = next((n for n in model.graph.node if n.op_type == "Add" and n.input[0] == node.output[0]), None)
+                if add_node:
+                    for initializer in model.graph.initializer:
+                        if initializer.name == add_node.input[1]:
+                            bias = onnx.numpy_helper.to_array(initializer)
+
+                activation_node = next((n for n in model.graph.node if n.input[0] == add_node.output[0]), None)
+
+                if weights is not None and bias is not None and activation_node:
+                    layer = Conv(
+                        kernel_params=(weights.shape[0], weights.shape[1], weights.shape[3]),
+                        act_func=activation_node.op_type.lower(),
+                        padding='valid' if node.pads == [0, 0, 0, 0] else 'full',
+                        stride=node.strides,
+                    )
+                    layer.weights = weights
+                    layer.bias = bias.flatten()
+                    layers.append(layer)
+            if node.op_type in ["MaxPool", "AveragePool"]:
+                layer = Pool(
+                    kernel_size=node.attribute[0].ints[0],
+                    stride=node.attribute[2].ints,
+                    padding='valid' if node.attribute[1].ints == [0, 0, 0, 0] else 'full',
+                    pool_mode="max" if node.op_type == "MaxPool" else "average",
+                )
+                layers.append(layer)
+            if node.op_type == "Dropout":
+                layers.append(Dropout(dropout_rate=node.attribute[0].f))
+            if node.op_type == "Reshape":
+                shape_tensor = next(t for t in model.graph.initializer if t.name == node.input[1])
+                shape = onnx.numpy_helper.to_array(shape_tensor)
+                layers.append(Reshape(output_shape=tuple(shape[1:])))
             if node.op_type == "MatMul":
                 weights = None
                 bias = None
